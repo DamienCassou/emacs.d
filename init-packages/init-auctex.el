8cab9c3c665b5d06c612fcf21fe11a4af50c2dac
Damien Cassou <damien.cassou@gmail.com>
2011-11-22 13:57:37 +0100

    Defines LaTeX-align-table inside init-auctex.el
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-packages/init-auctex.el
+++ init-packages/init-auctex.el
@@ -4,3 +4,13 @@
   '(add-to-list 'TeX-command-list
 		'("Bibtex all" "multibib/bibtexall" TeX-run-BibTeX
 		  nil t :help "Run Bibtex on all aux files") t))
+
+(eval-after-load "latex"
+  `(defun LaTeX-align-table ()
+     (interactive)
+     (save-excursion
+       (LaTeX-mark-environment)
+       (while (re-search-forward "& *" (region-end) t)
+	 (replace-match "& " nil nil))
+       (LaTeX-mark-environment)
+       (align-regexp (region-beginning) (region-end) "\\(\\s-*\\)\\(&\\|\\\\\\\\\\)" 1 1 t))))
