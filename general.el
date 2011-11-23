7167545ce7b29f3d937ee56d70f87b78a2e3933f
Damien Cassou <damien.cassou@gmail.com>
2011-11-23 20:22:54 +0100

    find-alternative-file-with-sudo is now provided by dired-toggle-sudo
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- general.el
+++ general.el
@@ -121,18 +121,6 @@ all get spell checked."
     (goto-char (cdr word-pos))))
 (global-set-key (kbd "M-@") 'my-mark-word)
 
-(defun find-alternative-file-with-sudo ()
-  (interactive)
-  (let ((fname (or buffer-file-name
-		   dired-directory)))
-    (when fname
-      (if (string-match "^/sudo:root@localhost:" fname)
-	  (setq fname (replace-regexp-in-string
-		       "^/sudo:root@localhost:" ""
-		       fname))
-	(setq fname (concat "/sudo:root@localhost:" fname)))
-      (find-alternate-file fname))))
-
 ;; misc functions
 (load "functions")
 
--- init-packages/init-dired-toggle-sudo.el
+++ init-packages/init-dired-toggle-sudo.el
@@ -1,2 +1,3 @@
 (require 'dired-toggle-sudo)
-(define-key dired-mode-map (kbd "C-c C-s") 'dired-toggle-sudo)
+;(define-key dired-mode-map (kbd "C-x s") 'dired-toggle-sudo)
+(global-set-key (kbd "C-x s") 'dired-toggle-sudo)
