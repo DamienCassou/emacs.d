7552b86deb69c5aed0da11a09026c6db415794bb
Damien Cassou <damien.cassou@gmail.com>
2011-12-10 12:40:39 +0100

    Cleans bashrc
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

---
+++ init-other-packages/init-bibtex.el
@@ -0,0 +1,11 @@
+(eval-after-load "bibtex"
+  '(add-to-list 'bibtex-BibTeX-entry-alist
+	     '("software" "A software" 
+	       (("title")
+		("version")
+		("organization")
+		("month")
+		("year")
+		("id"))
+	       nil
+	       nil)))
