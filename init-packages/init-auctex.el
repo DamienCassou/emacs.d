636c440530a31f0d601bac6e9970cb14650b3feb
Damien Cassou <damien.cassou@gmail.com>
2011-12-10 17:39:07 +0100

    Keybindings
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-packages/init-auctex.el
+++ init-packages/init-auctex.el
@@ -5,11 +5,13 @@
 		  nil t :help "Run Bibtex on all aux files") t))
 
 (eval-after-load "latex"
-  `(defun LaTeX-align-table ()
-     (interactive)
-     (save-excursion
-       (LaTeX-mark-environment)
-       (while (re-search-forward "& *" (region-end) t)
-	 (replace-match "& " nil nil))
-       (LaTeX-mark-environment)
-       (align-regexp (region-beginning) (region-end) "\\(\\s-*\\)\\(&\\|\\\\\\\\\\)" 1 1 t))))
+  `(progn
+     (defun LaTeX-align-table ()
+       (interactive)
+       (save-excursion
+	 (LaTeX-mark-environment)
+	 (while (re-search-forward "& *" (region-end) t)
+	   (replace-match "& " nil nil))
+	 (LaTeX-mark-environment)
+	 (align-regexp (region-beginning) (region-end) "\\(\\s-*\\)\\(&\\|\\\\\\\\\\)" 1 1 t)))
+     (define-key (eval 'TeX-mode-map) (kbd "s-a") #'LaTeX-align-table)))
--- init-packages/init-dired-toggle-sudo.el
+++ init-packages/init-dired-toggle-sudo.el
@@ -1,3 +1,2 @@
 (require 'dired-toggle-sudo)
-;(define-key dired-mode-map (kbd "C-x s") 'dired-toggle-sudo)
 (global-set-key (kbd "C-x s") 'dired-toggle-sudo)
