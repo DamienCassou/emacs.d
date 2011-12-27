7feae8129b6d4dbb744ac4d4a68140714c66e2b7
Damien Cassou <damien.cassou@gmail.com>
2011-12-27 12:58:07 +0100

    Moves spelling configuration to its own file
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- general.el
+++ general.el
@@ -30,44 +30,6 @@
 ;; The following lines are always needed.  Choose your own keys.
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
-(add-hook 'text-mode-hook 'flyspell-mode)
-
-;; ispell must ignore LaTeX commands and environments
-(setq ispell-tex-skip-alists
-      (list
-       (append (car ispell-tex-skip-alists)
-               '(("\\\\cite"            ispell-tex-arg-end)
-                 ("\\\\nocite"          ispell-tex-arg-end)
-                 ("\\\\includegraphics" ispell-tex-arg-end)
-                 ("\\\\figScale"         ispell-tex-arg-end)
-                 ("\\\\author"          ispell-tex-arg-end)
-                 ("\\\\ref"             ispell-tex-arg-end)
-                 ("\\\\eqref"             ispell-tex-arg-end)
-                 ("\\\\pageref"             ispell-tex-arg-end)
-                 ("\\\\label"           ispell-tex-arg-end)
-		 ("\\\\lstinputlisting" ispell-tex-arg-end)
-		 ("\\\\enword" ispell-tex-arg-end)
-		 ("\\\\ct" ispell-tex-arg-end)
-		 ("\\\\sigle" ispell-tex-arg-end)
-		 ("\\\\nocheck" ispell-tex-arg-end)
-		 ("\\\\mathit" ispell-tex-arg-end)
-		 ("\\\\url" ispell-tex-arg-end)
-		 ("\\\\lst[p]?\\(lignesa\\|lignes\\|ligne\\)" ispell-tex-arg-end 2)
-		 ("\\\\\\(lignesa\\|lignes\\|ligne\\)" ispell-tex-arg-end)
-		 ("\\\\c?cauthor" ispell-tex-arg-end)
-		 ("\\\\page" ispell-tex-arg-end)
-		 ("\\\\listing" ispell-tex-arg-end)
-		 ("\\\\macitationraw" ispell-tex-arg-end 2)
-		 ("\\\\macitation" ispell-tex-arg-end 3)
-		 ("\\\\mm" ispell-tex-arg-end)
-		 ("\\\\begin{lstlisting}" . "\\\\end{lstlisting}")
-		 ("\\\\begin{code}{}" . "\\\\end{code}")
-                 ))
-       (append (cadr ispell-tex-skip-alists)
-	       '(("tabular" ispell-tex-arg-end)
-		 ("equation\\*" . "\\\\end[ 	\n]*{[ 	\n]*equation\\*[ 	\n]*}")
-		 ("tikzpicture" . "\\\\end[ 	\n]*{[ 	\n]*tikzpicture[ 	\n]*}")))))
-
 ;; Make all "yes or no" prompts show "y or n" instead
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -110,3 +72,4 @@
 (load "init-ediff")
 (load "init-dired")
 (load "init-bibtex")
+(load "init-spelling")
