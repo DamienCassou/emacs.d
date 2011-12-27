21d11e3bd9827a2bbe795aa28a958372a0b83126
Damien Cassou <damien.cassou@gmail.com>
2011-12-27 12:58:23 +0100

    Some more cleaning for which-func
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-other-packages/init-which-func.el
+++ init-other-packages/init-which-func.el
@@ -3,13 +3,24 @@
 (add-to-list 'which-func-modes 'latex-mode)
 (add-to-list 'which-func-modes 'php-mode)
 
-;; Fixes which-function
+(defun which-func--limit (str)
+  "Returns a string that is equal to STR if STR's length is lower
+  than 20. Otherwise, returns the first 17 characters and append '...'."
+  (let ((limit 20))
+    (if (> (length str) limit)
+	(concat (substring str 0 17) "...")
+      str)))
+
 (defun which-func--trim (str)
   "Trim leading and tailing whitespace from STR."
   (let ((s (if (symbolp str) (symbol-name str) str)))
     (replace-regexp-in-string "\\(^[[:space:]\n]*\\|[[:space:]\n]*$\\)" "" s)))
 
-(defadvice which-function (around trim-which-function)
+(defun which-func--prepare (str)
+  "Cleans STR so that is can be displayed nicely within the mode line."
+  (which-func--limit (which-func--trim str)))
+
+(defadvice which-function (around prepare-which-function)
   ad-do-it
-  (setf ad-return-value (which-func--trim ad-return-value)))
+  (setf ad-return-value (which-func--prepare ad-return-value)))
 (ad-activate 'which-function)
