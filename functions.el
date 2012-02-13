a2c4db82916e75c50ea9256efc660c8bd42ebf3d
Damien Cassou <damien.cassou@gmail.com>
2012-02-13 13:22:14 +0100

    Add a function to kill all buffers
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- functions.el
+++ functions.el
@@ -42,3 +42,27 @@ If SUDO is not nil `method' is set to \"sudo\" and `user' to
   (shell (format "* Shell: %s *" default-directory)))
 
 (global-set-key (kbd "C-M-'") 'cw:shell:run)
+
+(let ((buffers-to-keep '("*scratch*" "*Messages*")))
+  (defun buffer-killable-p (buffer)
+    (and
+     (not (member (buffer-name buffer) buffers-to-keep))
+     (or (null (buffer-file-name buffer)) ;; buffer is not a file
+	 (not (buffer-modified-p buffer)))))) ;; or file is not modified
+
+(defun kill-all-buffers ()
+  (interactive)
+  (let ((count 0))
+    (dolist (buffer (buffer-list))
+      (message "Count is %s" count)
+      (when (buffer-killable-p buffer)
+	(incf count)
+	(message "Killing %s" (buffer-name buffer))
+	(kill-buffer buffer)))
+      (message "%s buffers have been killed" count)))
+
+
+
+;; Local Variables:
+;; lexical-binding: t
+;; End:
