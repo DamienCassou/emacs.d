846889a9f08df92a1901ca55002c65b5755ad47a
Damien Cassou <damien.cassou@gmail.com>
2012-04-18 16:27:04 +0200

    Default dir-locals.el file
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

---
+++ templates/.dir-locals.el
@@ -0,0 +1,13 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((latex-mode
+  (ispell-local-dictionary . "english")
+  (TeX-PDF-mode . t)
+  (TeX-master . "main")
+  (coding . utf-8)))
+
+
+
+
+
