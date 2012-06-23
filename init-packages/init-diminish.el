029eedc3efe811c6381b15173a0bd713c7feee7b
Damien Cassou <damien.cassou@gmail.com>
2012-06-23 11:56:49 +0200

    Refactor diminish configuration
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-packages/init-diminish.el
+++ init-packages/init-diminish.el
@@ -1,7 +1,10 @@
-(eval-after-load 'flyspell
-  '(progn
-     (diminish 'flyspell-mode)))
+(defmacro ignore-mode (package mode)
+  `(eval-after-load ',package
+     '(progn
+	(diminish ',mode))))
 
-(eval-after-load 'reftex-base
-  '(progn
-     (diminish 'reftex-mode)))
+(ignore-mode flyspell flyspell-mode)
+
+(ignore-mode reftex-base reftex-mode)
+
+(ignore-mode drag-stuff drag-stuff-mode)
