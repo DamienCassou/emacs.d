095e1aee2e32872f7c5d415ed52bd89ea57e2d05
Damien Cassou <damien.cassou@gmail.com>
2012-07-01 12:50:11 +0200

    Add a keybinding for shell-switcher-other-window
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-packages/init-shell-switcher.el
+++ init-packages/init-shell-switcher.el
@@ -1,2 +1,3 @@
 (global-set-key (kbd "C-'")   'shell-switcher-switch-buffer)
+(global-set-key (kbd "C-x 4 '") 'shell-switcher-switch-buffer-other-window)
 (global-set-key (kbd "C-M-'") 'shell-switcher-new-shell)
