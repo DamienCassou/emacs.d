2b063b193ec420130f29b4af7bf91b65c1fccc80
Damien Cassou <damien.cassou@gmail.com>
2012-07-04 14:37:58 +0200

    Set up winner mode so I can press <C-c left left left>
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

---
+++ init-other-packages/init-winner.el
@@ -0,0 +1,22 @@
+(defun winner:prepare-for-futher-undo ()
+  "Let one undo more by just pressing the last key.
+Using standard configuration and this function, the user will be
+able to type <C-c left left left> to undo 3 times whereas it was
+<C-c left C-c left C-c left> before."
+  (let* ((repeat-key (event-basic-type last-input-event))
+	 (repeat-key-str (format-kbd-macro (vector repeat-key)))
+	 (message (format "Type %s again to continue switching"
+			  (format-kbd-macro (vector repeat-key)))))
+    (set-temporary-overlay-map
+     (let ((map (make-sparse-keymap)))
+       (define-key map (vector repeat-key)
+	 `(lambda () (interactive)
+	    (winner-undo)
+	    (message ,message)))
+       map) t)
+    (message message)))
+
+(define-key winner-mode-map (kbd "C-c <left>") (lambda ()
+						 (interactive)
+						 (winner-undo)
+						 (winner:prepare-for-futher-undo)))
