2ed10831cd346955b1c320d1d1860d283e5b2cc6
Damien Cassou <damien.cassou@gmail.com>
2012-07-12 14:43:06 +0200

    Make drag-stuff and rebase-mode compatible
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-packages/init-magit.el
+++ init-packages/init-magit.el
@@ -3,3 +3,9 @@
 
 (require 'magit-svn)
 (add-hook 'magit-mode-hook 'turn-on-magit-svn)
+
+(eval-after-load "drag-stuff"
+  (eval-after-load "rebase-mode"
+    '(progn
+       (define-key rebase-mode-map (drag-stuff--kbd 'up) 'rebase-mode-move-line-up)
+       (define-key rebase-mode-map (drag-stuff--kbd 'down) 'rebase-mode-move-line-down))))
