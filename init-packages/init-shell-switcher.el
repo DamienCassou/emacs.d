3ee20ce466b1feed18dd2db66bc01dc4dea71019
Damien Cassou <damien.cassou@gmail.com>
2012-07-15 19:36:22 +0200

    Nothing to initialize shell-switcher anymore except customized variables
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-packages/init-shell-switcher.el
+++
@@ -1,3 +0,0 @@
-(global-set-key (kbd "C-'")   'shell-switcher-switch-buffer)
-(global-set-key (kbd "C-x 4 '") 'shell-switcher-switch-buffer-other-window)
-(global-set-key (kbd "C-M-'") 'shell-switcher-new-shell)
