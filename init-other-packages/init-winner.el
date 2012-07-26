98aa62beb4329b79459a9d4c2362bfb377daf1e6
Damien Cassou <damien.cassou@gmail.com>
2012-07-26 19:09:02 +0200

    Cleaning
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-other-packages/init-winner.el
+++ init-other-packages/init-winner.el
@@ -16,7 +16,8 @@ able to type <C-c left left left> to undo 3 times whereas it was
        map) t)
     (message message)))
 
-(define-key winner-mode-map (kbd "C-c <left>") (lambda ()
-						 (interactive)
-						 (winner-undo)
-						 (winner:prepare-for-futher-undo)))
+(define-key winner-mode-map (kbd "C-c <left>")
+  (lambda ()
+    (interactive)
+    (winner-undo)
+    (winner:prepare-for-futher-undo)))
