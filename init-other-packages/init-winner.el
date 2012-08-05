662f0c2390bb76799e73af6ea03b1b859da37c91
Damien Cassou <damien.cassou@gmail.com>
2012-08-05 19:23:17 +0200

    Improve winner mode
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-other-packages/init-winner.el
+++ init-other-packages/init-winner.el
@@ -3,21 +3,23 @@
 Using standard configuration and this function, the user will be
 able to type <C-c left left left> to undo 3 times whereas it was
 <C-c left C-c left C-c left> before."
-  (let* ((repeat-key (event-basic-type last-input-event))
-	 (repeat-key-str (format-kbd-macro (vector repeat-key)))
-	 (message (format "Type %s again to continue switching"
-			  (format-kbd-macro (vector repeat-key)))))
+  (let* ((message (format "Type <left>/<right> to continue switching")))
     (set-temporary-overlay-map
      (let ((map (make-sparse-keymap)))
-       (define-key map (vector repeat-key)
+       (define-key map (kbd "<left>")
 	 `(lambda () (interactive)
-	    (winner-undo)
-	    (message ,message)))
+	    (setq this-command 'winner-undo)
+	    (winner-undo)))
+       (define-key map (kbd "<right>")
+	 `(lambda () (interactive)
+	    (setq this-command 'winner-undo)
+	    (winner-redo)))
        map) t)
     (message message)))
 
 (define-key winner-mode-map (kbd "C-c <left>")
   (lambda ()
     (interactive)
+    (setq this-command 'winner-undo)
     (winner-undo)
     (winner:prepare-for-futher-undo)))
