4417df0b5b860b6a54615da5c5ef175fdad19128
Damien Cassou <damien.cassou@gmail.com>
2012-08-28 10:56:06 +0200

    Decompose winner functions to debug
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-other-packages/init-winner.el
+++ init-other-packages/init-winner.el
@@ -1,25 +1,30 @@
+(defun winner:undo-again ()
+  (interactive)
+  (setq this-command 'winner-undo)
+  (winner-undo))
+
+(defun winner:redo-again ()
+  (interactive)
+  (setq this-command 'winner-undo)
+  (winner-redo))
+
 (defun winner:prepare-for-futher-undo ()
   "Let one undo more by just pressing the last key.
 Using standard configuration and this function, the user will be
 able to type <C-c left left left> to undo 3 times whereas it was
 <C-c left C-c left C-c left> before."
-  (let* ((message (format "Type <left>/<right> to continue switching")))
-    (set-temporary-overlay-map
-     (let ((map (make-sparse-keymap)))
-       (define-key map (kbd "<left>")
-	 `(lambda () (interactive)
-	    (setq this-command 'winner-undo)
-	    (winner-undo)))
-       (define-key map (kbd "<right>")
-	 `(lambda () (interactive)
-	    (setq this-command 'winner-undo)
-	    (winner-redo)))
-       map) t)
-    (message message)))
+  (set-temporary-overlay-map
+   (let ((map (make-sparse-keymap)))
+     (define-key map (kbd "<left>") 'winner:undo-again)
+     (define-key map (kbd "<right>") 'winner:redo-again)
+     map) t)
+  (message "Type <left>/<right> to continue switching"))
 
-(define-key winner-mode-map (kbd "C-c <left>")
-  (lambda ()
+(defun winner:initial-undo ()
     (interactive)
     (setq this-command 'winner-undo)
     (winner-undo)
-    (winner:prepare-for-futher-undo)))
+    (winner:prepare-for-futher-undo))
+
+(define-key winner-mode-map (kbd "C-c <left>")
+  'winner:initial-undo)
