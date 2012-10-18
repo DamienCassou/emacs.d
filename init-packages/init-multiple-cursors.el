c6f0f34b9998a2007aa23a2769d2d0d2260afa6e
Damien Cassou <damien.cassou@gmail.com>
2012-10-18 14:38:20 +0200

    Try multiple-cursors
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

---
+++ init-packages/init-multiple-cursors.el
@@ -0,0 +1,6 @@
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C-x ]") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-x [") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
