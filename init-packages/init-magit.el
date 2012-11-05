c5e92c22aa64e8d60c6f67a08f1e742a514c67a9
Damien Cassou <damien.cassou@gmail.com>
2012-11-05 18:42:45 +0100

    Add missing quote
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init-packages/init-magit.el
+++ init-packages/init-magit.el
@@ -4,8 +4,9 @@
 (require 'magit-svn)
 (add-hook 'magit-mode-hook 'turn-on-magit-svn)
 
+;; Use shortcuts of drag-stuff mode to move lines up/down in rebase-mode
 (eval-after-load "drag-stuff"
-  (eval-after-load "rebase-mode"
-    '(progn
-       (define-key rebase-mode-map (drag-stuff--kbd 'up) 'rebase-mode-move-line-up)
-       (define-key rebase-mode-map (drag-stuff--kbd 'down) 'rebase-mode-move-line-down))))
+  '(eval-after-load "rebase-mode"
+     '(progn
+        (define-key rebase-mode-map (drag-stuff--kbd 'up) 'rebase-mode-move-line-up)
+        (define-key rebase-mode-map (drag-stuff--kbd 'down) 'rebase-mode-move-line-down))))
