810513a9a27cbd3f0eca2b68fe64bfaca9378c9c
Damien Cassou <damien.cassou@gmail.com>
2013-01-08 17:09:56 +0100

    Add toggle-window-split and bind it to C-x 4 t
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- functions.el
+++ functions.el
@@ -1,3 +1,30 @@
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+(define-key ctl-x-4-map "t" 'toggle-window-split)
+
 (defun my:update-autoloads ()
   "Manually call this command when you want to update autoloads"
   (interactive)
