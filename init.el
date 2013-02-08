a1529ce496f536057749ac3fc1c5a8288c5ad208
Damien Cassou <damien.cassou@gmail.com>
2013-02-08 15:06:41 +0100

    Playing with Mobile org, but not yet convinced
    Signed-off-by: Damien Cassou <damien.cassou@gmail.com>

--- init.el
+++ init.el
@@ -86,6 +86,8 @@
  '(message-log-max t)
  '(next-screen-context-lines 5)
  '(notmuch-labeler-hide-known-labels t)
+ '(org-mobile-directory "~/Dropbox/MobileOrg/")
+ '(org-mobile-inbox-for-pull "~/Documents/configuration/org/from-mobile.org")
  '(proced-filter (quote all))
  '(read-file-name-completion-ignore-case t)
  '(recentf-auto-cleanup 300)
