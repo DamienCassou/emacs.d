f3e6937f23e698a2c35d20cb1eb2ac489976e722
Damien Cassou <damien.cassou@gmail.com>
2013-12-08 10:42:11 +0100

    Try mu4e

--- init.el
+++ init.el
@@ -870,3 +870,55 @@ able to type <C-c left left left> to undo 3 times whereas it was
   :init
   (progn
     (projectile-global-mode)))
+
+(use-package mu4e
+  :config
+  (progn
+    (setq mu4e-mu-binary "~/.emacs.d/packages/mu/mu/mu")
+    (setq mu4e-maildir       "~/Mail/Gmail")
+    (setq mu4e-drafts-folder "/[Gmail].Drafts")
+    (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+    (setq mu4e-trash-folder  "/[Gmail].Trash")
+
+    ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+    (setq mu4e-sent-messages-behavior 'delete)
+
+    ;; setup some handy shortcuts
+    ;; you can quickly switch to your Inbox -- press ``ji''
+    ;; then, when you want archive some messages, move them to
+    ;; the 'All Mail' folder by pressing ``ma''.
+
+    (setq mu4e-maildir-shortcuts
+          '( ("/INBOX"               . ?i)
+             ("/[Gmail].Sent Mail"   . ?s)
+             ("/[Gmail].Trash"       . ?t)
+             ("/[Gmail].All Mail"    . ?a)))
+
+    ;; allow for updating mail using 'U' in the main view:
+    (setq mu4e-get-mail-command "offlineimap")
+
+    ;; something about ourselves
+    (setq
+     user-mail-address "damien.cassou@gmail.com"
+     user-full-name  "Damien Cassou"
+     message-signature
+     (concat
+      "Foo X. Bar\n"
+      "http://www.example.com\n"))
+
+    ;; sending mail -- replace USERNAME with your gmail username
+    ;; also, make sure the gnutls command line utils are installed
+    ;; package 'gnutls-bin' in Debian/Ubuntu
+
+    (require 'smtpmail)
+
+    ;; alternatively, for emacs-24 you can use:
+    (setq
+     message-send-mail-function 'smtpmail-send-it
+     smtpmail-stream-type 'starttls
+     smtpmail-default-smtp-server "smtp.gmail.com"
+     smtpmail-smtp-server "smtp.gmail.com"
+     smtpmail-smtp-service 587)
+
+    ;; don't keep message buffers around
+    (setq message-kill-buffer-on-exit t)))
