d66f707a3b183387217f4467b994512200b3c25d
Damien Cassou <damien.cassou@gmail.com>
2014-04-02 14:33:21 +0200

    Utility function for Pillar

--- init.el
+++ init.el
@@ -1211,63 +1211,46 @@ able to type <C-c left left left> to undo 3 times whereas it was
     (add-hook 'prog-mode-hook 'company-mode)
     (bind-key "C-. y" 'company-yasnippet)))
 
-
-
 (defun update-pillar-image ()
   (interactive)
   (require 'f)
   (require 's)
   (require 'dash)
 
-  (defvar images-dir "~/Documents/smalltalk/images/")
+  (let* ((images-dir "~/Documents/smalltalk/images/")
+         (pillar-directories
+          (-map #'f-filename (f-directories images-dir
+                                            (lambda (dir)
+                                              (s-starts-with? "pillar" (f-filename dir)))
+                                            t)))
 
-  (defvar pillar-directories
-    (-map #'f-filename (f-directories
-                        images-dir
-                        (lambda (dir)
-                          (s-starts-with? "pillar" (f-filename dir)))
-                        t)))
-
-  (defvar pillar-directory
-    (cond
-     ((null pillar-directories)
-      (error "No pillar image found in %s" images-dir))
-     ((= 1 (length pillar-directories))
-      (car pillar-directories))
-     (t (completing-read "Image: " pillar-directories nil t nil nil
-                         (car pillar-directories)))))
-
-  (defvar pillar-image
-    (f-expand (s-concat pillar-directory ".image")
-              (f-expand pillar-directory images-dir)))
-
-  (unless (f-exists? pillar-image)
-    (error "No image at %s" pillar-image))
-
-  (defvar pillar-changes
-    (s-concat (f-no-ext pillar-image) ".changes"))
-
-  (unless (f-exists? pillar-changes)
-    (error "No changes at %s" pillar-changes))
-
-
-  (defvar destination-image
-    (f-expand "Pharo.image" default-directory))
-
-  (defvar destination-changes
-    (f-expand "Pharo.changes" default-directory))
-
-  (when (f-exists? destination-image)
-    (if (yes-or-no-p (format "Are you sure you want to delete %s? "
-                             destination-image))
-        (progn
-          (f-delete destination-image)
-          (f-delete destination-changes))
-      (error "Can't continue without deleting existing image %s"
-             destination-image)))
-
-  (f-symlink pillar-image destination-image)
-  (f-symlink pillar-changes destination-changes))
+         (pillar-directory (cond
+                            ((null pillar-directories)
+                             (error "No pillar image found in %s" images-dir))
+                            ((= 1 (length pillar-directories))
+                             (car pillar-directories))
+                            (t (completing-read "Image: " pillar-directories nil t nil nil
+                                                (car pillar-directories)))))
+         (pillar-image (f-expand (s-concat pillar-directory ".image")
+                                 (f-expand pillar-directory images-dir)))
+         (pillar-changes (s-concat (f-no-ext pillar-image) ".changes"))
+         (destination-image (f-expand "Pharo.image" default-directory))
+         (destination-changes (f-expand "Pharo.changes" default-directory)))
+    (unless (f-exists? pillar-image)
+      (error "No image at %s" pillar-image))
+    (unless (f-exists? pillar-changes)
+      (error "No changes at %s" pillar-changes))
+    (when (or (f-exists? destination-image)
+              (f-symlink? destination-image))
+      (if (yes-or-no-p (format "Are you sure you want to delete %s? "
+                               destination-image))
+          (progn
+            (f-delete destination-image)
+            (f-delete destination-changes))
+        (error "Can't continue without deleting existing image %s"
+               destination-image)))
+    (f-symlink pillar-image destination-image)
+    (f-symlink pillar-changes destination-changes)))
 
 (use-package-with-elapsed-timer "Starting server"
   (server-start))
