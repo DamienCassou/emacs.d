cd5575d6cba367c1468301a7af2a25a068a576b0
Damien Cassou <damien@cassou.me>
2015-09-14 12:41:11 +0200

    Remove 3 obsolete files

--- snippets/pillar-mode/.yas-compiled-snippets.el
+++
@@ -1,9 +0,0 @@
-;;; Compiled snippets and support files for `pillar-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'pillar-mode
-                     '(("picture" "+${1:A caption}>file://`(file-relative-name (read-file-name \"Select a picture file \" nil \"figures/picture.png\" 'confirm-after-completion))`${2:|label=${3:figName}}${4:|width=${5:50}}+\n" "picture" nil nil nil nil nil nil)
-                       ("[[[" "[[[${1:language=${2:$$(yas-choose-value '(\"Smalltalk\" \"HTML\" \"HTTP\" \"Bash\"))}}${3:|label=${4:script1}}${5:|caption=${6:A description of this script}}\n$0\n]]]" "scripts" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Thu Jan 30 14:55:59 2014
