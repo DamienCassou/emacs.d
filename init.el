72c368351bf049fd734d4290e5cf1c4cf037db10
Damien Cassou <damien@cassou.me>
2016-01-05 17:39:19 +0100

    Try magit-gh-pulls

--- init.el
+++ init.el
@@ -670,39 +670,10 @@ narrowed."
        (list "*.aux" "*.log" "*.out" "*.bbl" "*.blg" "auto/" "*.synctex.gz" "*.toc"))
       (magit-refresh))
 
-    ;; http://endlessparentheses.com/easily-create-github-prs-from-magit.html
-    ;; http://endlessparentheses.com/create-github-prs-from-emacs-with-magit.html
-    (defun endless/visit-pull-request-url ()
-      "Visit the current branch's PR on Github."
-      (interactive)
-      (browse-url
-       (format "https://github.com/%s/pull/new/%s"
-               (replace-regexp-in-string
-                "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
-                (magit-get "remote"
-                           (magit-get-remote)
-                           "url"))
-               (cdr (magit-get-remote-branch)))))
-
-    (define-key magit-mode-map "v"
-      #'endless/visit-pull-request-url)
-
-    ;; http://endlessparentheses.com/automatically-configure-magit-to-access-github-prs.html
-    (defun endless/add-PR-fetch ()
-      "If refs/pull is not defined on a GH repo, define it."
-      (let ((fetch-address
-             "+refs/pull/*/head:refs/pull/origin/*")
-            (magit-remotes
-             (magit-get-all "remote" "origin" "fetch")))
-        (unless (or (not magit-remotes)
-                    (member fetch-address magit-remotes))
-          (when (string-match
-                 "github" (magit-get "remote" "origin" "url"))
-            (magit-git-string
-             "config" "--add" "remote.origin.fetch"
-             fetch-address)))))
-
-    (add-hook 'magit-mode-hook #'endless/add-PR-fetch)))
+    (use-package magit-gh-pulls
+      :init
+      (progn
+        (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)))))
 
 (use-package info
   :bind ("C-h i" . my:info-other-window)
