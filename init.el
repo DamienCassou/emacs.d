96acae7789777c01d67ab01950694c2423436818
Damien Cassou <damien@cassou.me>
2016-02-24 14:07:25 +0100

    Try to fix initialization problem of ispell

--- init.el
+++ init.el
@@ -706,8 +706,6 @@ narrowed."
          (define-key help-mode-map "o" 'ace-link-help)))))
 
 (use-package ispell
-  :defer t
-  :requires flyspell
   :bind
   (("C-. d b"   . ispell-buffer)
    ("C-. d f" . ispell-change-dictionary-to-french)
@@ -739,47 +737,46 @@ narrowed."
       (let ((mode-value (if flyspell-mode -1 1)))
         (save-excursion
           (add-file-local-variable 'eval `(flyspell-mode ,mode-value)))
-        (flyspell-mode mode-value)))
-    )
+        (flyspell-mode mode-value))))
   :config
   ;; ispell must ignore LaTeX commands and environments
-  (setq ispell-tex-skip-alists
-        (list
-         (append (car ispell-tex-skip-alists)
-                 '(("\\\\cite"            ispell-tex-arg-end)
-                   ("\\\\nocite"          ispell-tex-arg-end)
-                   ("\\\\includegraphics" ispell-tex-arg-end)
-                   ("\\\\figScale"         ispell-tex-arg-end)
-                   ("\\\\author"          ispell-tex-arg-end)
-                   ("\\\\ref"             ispell-tex-arg-end)
-                   ("\\\\eqref"             ispell-tex-arg-end)
-                   ("\\\\pageref"             ispell-tex-arg-end)
-                   ("\\\\label"           ispell-tex-arg-end)
-                   ("\\\\lstinputlisting" ispell-tex-arg-end)
-                   ("\\\\enword" ispell-tex-arg-end)
-                   ("\\\\ct" ispell-tex-arg-end)
-                   ("\\\\sigle" ispell-tex-arg-end)
-                   ("\\\\nocheck" ispell-tex-arg-end)
-                   ("\\\\mathit" ispell-tex-arg-end)
-                   ("\\\\url" ispell-tex-arg-end)
-                   ("\\\\lst[p]?\\(lignesa\\|lignes\\|ligne\\)" ispell-tex-arg-end 2)
-                   ("\\\\\\(lignesa\\|lignes\\|ligne\\)" ispell-tex-arg-end)
-                   ("\\\\c?cauthor" ispell-tex-arg-end)
-                   ("\\\\page" ispell-tex-arg-end)
-                   ("\\\\listing" ispell-tex-arg-end)
-                   ("\\\\macitationraw" ispell-tex-arg-end 2)
-                   ("\\\\macitation" ispell-tex-arg-end 3)
-                   ("\\\\mm" ispell-tex-arg-end)
-                   ("\\\\begin{lstlisting}" . "\\\\end{lstlisting}")
-                   ("\\\\begin{code}{}" . "\\\\end{code}")
-                   ))
-         (append (cadr ispell-tex-skip-alists)
-                 '(("tabular" ispell-tex-arg-end)
-                   ("equation\\*" . "\\\\end[ 	\n]*{[ 	\n]*equation\\*[ 	\n]*}")
-                   ("tikzpicture" . "\\\\end[ 	\n]*{[ 	\n]*tikzpicture[ 	\n]*}"))))))
+  (progn
+    (setq ispell-tex-skip-alists
+          (list
+           (append (car ispell-tex-skip-alists)
+                   '(("\\\\cite"            ispell-tex-arg-end)
+                     ("\\\\nocite"          ispell-tex-arg-end)
+                     ("\\\\includegraphics" ispell-tex-arg-end)
+                     ("\\\\figScale"         ispell-tex-arg-end)
+                     ("\\\\author"          ispell-tex-arg-end)
+                     ("\\\\ref"             ispell-tex-arg-end)
+                     ("\\\\eqref"             ispell-tex-arg-end)
+                     ("\\\\pageref"             ispell-tex-arg-end)
+                     ("\\\\label"           ispell-tex-arg-end)
+                     ("\\\\lstinputlisting" ispell-tex-arg-end)
+                     ("\\\\enword" ispell-tex-arg-end)
+                     ("\\\\ct" ispell-tex-arg-end)
+                     ("\\\\sigle" ispell-tex-arg-end)
+                     ("\\\\nocheck" ispell-tex-arg-end)
+                     ("\\\\mathit" ispell-tex-arg-end)
+                     ("\\\\url" ispell-tex-arg-end)
+                     ("\\\\lst[p]?\\(lignesa\\|lignes\\|ligne\\)" ispell-tex-arg-end 2)
+                     ("\\\\\\(lignesa\\|lignes\\|ligne\\)" ispell-tex-arg-end)
+                     ("\\\\c?cauthor" ispell-tex-arg-end)
+                     ("\\\\page" ispell-tex-arg-end)
+                     ("\\\\listing" ispell-tex-arg-end)
+                     ("\\\\macitationraw" ispell-tex-arg-end 2)
+                     ("\\\\macitation" ispell-tex-arg-end 3)
+                     ("\\\\mm" ispell-tex-arg-end)
+                     ("\\\\begin{lstlisting}" . "\\\\end{lstlisting}")
+                     ("\\\\begin{code}{}" . "\\\\end{code}")
+                     ))
+           (append (cadr ispell-tex-skip-alists)
+                   '(("tabular" ispell-tex-arg-end)
+                     ("equation\\*" . "\\\\end[ 	\n]*{[ 	\n]*equation\\*[ 	\n]*}")
+                     ("tikzpicture" . "\\\\end[ 	\n]*{[ 	\n]*tikzpicture[ 	\n]*}")))))))
 
 (use-package flyspell
-  :defer t
   :diminish flyspell-mode
   :bind (("C-. f b" . flyspell-buffer)
          ("C-. f m" . flyspell-toggle))
