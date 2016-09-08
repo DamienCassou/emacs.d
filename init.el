b23749cd04f1dccf9498ee824a15b9a210ea3580
Damien Cassou <damien@cassou.me>
2016-09-08 10:07:07 +0200

    Add some noisy mailing lists

--- init.el
+++ init.el
@@ -1108,7 +1108,7 @@ able to type <C-c left left left> to undo 3 times whereas it was
            ("damien.cassou@lifl.fr" "cassou@inria.fr"
             "damien.cassou@laposte.net")))
     (setq profile-noisy-query
-          "list:\"pharo-dev\" OR list:\"pharo-users\" OR list:smallwiki OR to:notmuch@notmuchmail.org OR to:offlineimap-project@lists.alioth.debian.org OR list:nix-dev")))
+          "list:\"pharo-dev\" OR list:\"pharo-users\" OR list:\"smallwiki\" OR to:\"notmuch@notmuchmail.org\" OR to:\"offlineimap-project@lists.alioth.debian.org\" OR list:\"nix-dev\" OR \"cristal.univ-lille1.fr\" OR \"tous-lille.inria.fr\" OR \"online.inria.fr\" OR \"tous-les-personnels.univ-lille1.fr\" OR \"cristal-cr-cdl.univ-lille1.fr\" OR \"cristal-permanents.univ-lille1.fr\"")))
 
 (use-package mml
   :defer t
