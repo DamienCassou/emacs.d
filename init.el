40abd463ea66da38f089496e07411bfad94cf101
Damien Cassou <damien@cassou.me>
2016-10-30 11:00:24 +0100

    js-mode has better line movement than drag-stuff

--- init.el
+++ init.el
@@ -619,6 +619,7 @@ able to type <C-c left left left> to undo 3 times whereas it was
   (progn
     (drag-stuff-global-mode t)
     (add-to-list 'drag-stuff-except-modes 'org-mode)
+    (add-to-list 'drag-stuff-except-modes 'js-mode)
     (add-to-list 'drag-stuff-except-modes 'rebase-mode)))
 
 (use-package expand-region
