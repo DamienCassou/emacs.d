16e12d81d1529b9f60171b09b006610b06e19e1f
Damien Cassou <damien@cassou.me>
2016-11-27 08:00:04 +0100

    Switch from nixos to fedora

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
  '(electric-pair-mode t)
  '(enable-local-variables :all)
  '(enable-recursive-minibuffers t)
- '(erc-autojoin-channels-alist (quote (("freenode.net" "#emacs" "#nixos"))))
+ '(erc-autojoin-channels-alist (quote (("freenode.net" "#fedora" "#emacs"))))
  '(erc-nick "DamienCassou")
  '(eval-expression-print-length 20)
  '(eval-expression-print-level nil)
