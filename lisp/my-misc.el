be305582e81eb3dd57e371cb102a1099e9fe97df
Damien Cassou <damien@cassou.me>
2017-03-20 10:59:54 +0100

    Add my/dired-rsync

--- lisp/my-misc.el
+++ lisp/my-misc.el
@@ -160,9 +160,46 @@ Interactively, unmount when prefix argument."
 
 (put 'narrow-to-region 'disabled nil)
 
+;; https://oremacs.com/2016/02/24/dired-rsync/
+;;;###autoload
+(defun my/dired-rsync (dest)
+  (interactive
+   (list
+    (expand-file-name
+     (read-file-name
+      "Rsync to:"
+      (dired-dwim-target-directory)))))
+  ;; store all selected files into "files" list
+  (let ((files (dired-get-marked-files
+                nil current-prefix-arg))
+        ;; the rsync command
+        (tmtxt/rsync-command
+         "rsync -arvz --progress "))
+    ;; add all selected file names as arguments
+    ;; to the rsync command
+    (dolist (file files)
+      (setq tmtxt/rsync-command
+            (concat tmtxt/rsync-command
+                    (shell-quote-argument file)
+                    " ")))
+    ;; append the destination
+    (setq tmtxt/rsync-command
+          (concat tmtxt/rsync-command
+                  (shell-quote-argument dest)))
+    ;; run the async shell command
+    (async-shell-command tmtxt/rsync-command "*rsync*")
+    ;; finally, switch to that window
+    (other-window 1)))
+
+(define-key dired-mode-map "Y" 'ora-dired-rsync)
+
 (require 'server)
 (unless (or (daemonp) (server-running-p))
   (server-start))
 
 (provide 'my-misc)
 ;;; my-misc.el ends here
+
+;; Local Variables:
+;; eval: (flycheck-mode -1)
+;; End:
