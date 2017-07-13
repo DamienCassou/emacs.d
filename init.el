fd38873f101fb6fb17efacc9faf3316fd97abb9f
Damien Cassou <damien@cassou.me>
2017-07-13 22:43:34 +0200

    Add ignored package-initialize

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;;; init.el --- user-init-file                    -*- lexical-binding: t -*-
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (setq package-enable-at-startup nil)
 
 (progn ;    `borg'
