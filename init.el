1bf26bbbfda8b946533bb8e6d45e4ee13a664622
Damien Cassou <damien@cassou.me>
2017-08-16 09:23:31 +0200

    Update magit status sections definition

--- init.el
+++ init.el
@@ -276,15 +276,8 @@
     (global-magit-file-mode)
     ;; Enable magit-clean
     (put 'magit-clean 'disabled nil)
-
     (magit-add-section-hook 'magit-status-sections-hook
-                            'magit-insert-modules-unpulled-from-upstream
-                            'magit-insert-unpulled-from-upstream)
-    (magit-add-section-hook 'magit-status-sections-hook
-                            'magit-insert-modules-unpushed-to-upstream
-                            'magit-insert-unpulled-from-upstream)
-    (magit-add-section-hook 'magit-status-sections-hook
-                            'magit-insert-modules-unpushed-to-pushremote
+                            'magit-insert-modules
                             'magit-insert-unpulled-from-upstream)))
 
 (use-package vc-hooks
