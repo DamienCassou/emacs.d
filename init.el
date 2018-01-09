3ca927480276d0c4c7724b04ddee7349f9b146da
Damien Cassou <damien@cassou.me>
2018-01-09 17:06:36 +0100

    Remove space and ; from avy keys to see if this improves readability

--- init.el
+++ init.el
@@ -1124,7 +1124,7 @@ Designed to be called before `message-send-and-exit'."
   :init
   (progn
     ;; keys under my fingers (blue keys on my Kinesis Advantage)
-    (setq aw-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o 59 32))
+    (setq aw-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o))
     (setq aw-scope 'frame)))
 
 (use-package avy
@@ -1133,7 +1133,7 @@ Designed to be called before `message-send-and-exit'."
   :init
   (progn
     ;; keys under my fingers (blue keys on my Kinesis Advantage)
-    (setq avy-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o 59 32))
+    (setq avy-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o))
     (setq avy-style 'at-full)))
 
 (use-package outline
