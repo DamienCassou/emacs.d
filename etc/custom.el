ee77850ad95c85f0f291564065e87e9a634681b5
Damien Cassou <damien@cassou.me>
2018-01-23 08:43:39 +0100

    I have no clue

--- etc/custom.el
+++ etc/custom.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bookmark-save-flag 1 t)
+ '(bookmark-save-flag 1)
  '(custom-safe-themes
    (quote
     ("de0c2b8761eecf8f310b4b1873e4542abe107a83f573ba4690faee11aadcea8b")))
