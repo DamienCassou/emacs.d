f3948fcc381e74f2c060040cab05fb9b10c239fe
Damien Cassou <damien@cassou.me>
2018-03-12 10:19:35 +0100

    Try use-package statistics

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
   (setq use-package-enable-imenu-support t)
   (setq use-package-minimum-reported-time 0)
   (setq use-package-verbose t)
+  (setq use-package-compute-statistics t)
   (require 'use-package))
 
 (use-package auto-compile
