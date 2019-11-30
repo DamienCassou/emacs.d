502d2eeb0a6e4dafdd2fed4bdc958cfc636effd7
Damien Cassou <damien@cassou.me>
2019-11-30 13:56:55 +0100

    ag/rg: add initial input to filter the search

--- init.el
+++ init.el
@@ -1295,6 +1295,10 @@ I.e., the keyring has a public key for each recipient."
      `(("t" ,(my/apply-bookmark-fn #'my/counsel-open-in-external-terminal) "open terminal")
        ("s" ,(my/apply-bookmark-fn #'my/counsel-open-in-eshell) "eshell")))
 
+    ;; http://xenodium.com/emacs-counsel-default-search-switches/index.html
+    (ivy-configure 'counsel-ag :initial-input "--file-search-regex '' -- ")
+    (ivy-configure 'counsel-rg :initial-input "--glob '**' -- ")
+
     (counsel-mode)))
 
 (use-package ivy
@@ -1321,6 +1325,10 @@ I.e., the keyring has a public key for each recipient."
 (use-package counsel-projectile
   :demand t
   :after projectile
+  :init
+  (progn
+    ;; http://xenodium.com/emacs-counsel-default-search-switches/index.html
+    (setq counsel-projectile-rg-initial-input "--glob '**' -- "))
   :config
   (progn
     (counsel-projectile-mode)
