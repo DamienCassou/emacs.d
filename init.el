6ad2eee70c1ee1f4763b9532ba4dc5eb19681e33
Damien Cassou <damien@cassou.me>
2019-12-02 04:33:20 +0100

    Revert "unfill: disable as it seems broken in Emacs 27 apparently"
    This reverts commit 8aed71767a150807c6fec34585824183dbcfc834.

--- init.el
+++ init.el
@@ -1994,7 +1994,6 @@ I.e., the keyring has a public key for each recipient."
     (setq youtube-dl-directory (expand-file-name "~/Downloads/"))))
 
 (use-package unfill
-  :disabled t
   :bind ([remap fill-paragraph] . unfill-toggle))
 
 (use-package transmission
