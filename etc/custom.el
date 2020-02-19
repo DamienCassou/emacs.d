1cef2e22585ea552a93db356e960a010b4b48609
Damien Cassou <damien@cassou.me>
2020-02-19 10:04:15 +0100

    custom: safe variables

--- etc/custom.el
+++ etc/custom.el
@@ -21,7 +21,8 @@
  '(menu-bar-mode nil)
  '(next-screen-context-lines 5)
  '(safe-local-variable-values
-   '((org-export-initial-scope . buffer)
+   '((TeX-master . "geiser")
+     (org-export-initial-scope . buffer)
      (org-id-link-to-org-use-id)
      (org-export-with-broken-links . t)
      (eval require 'org-make-toc)
