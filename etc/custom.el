b8377d4a07662468b08cfd5ff35e99c35e5415f6
Damien Cassou <damien@cassou.me>
2020-04-12 18:26:56 +0200

    Fix widgetjs faces to improve rendering with modus-operandi theme

--- etc/custom.el
+++ etc/custom.el
@@ -119,7 +119,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(widgetjs-html-tag-face ((t (:foreground "#61afef" :box 1 :weight bold)))))
 
 ;; Local Variables:
 ;; eval: (flycheck-mode -1)
