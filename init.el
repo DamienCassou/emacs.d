6ed7334ba29952301c32a4fed499e2e74318e004
Damien Cassou <damien@cassou.me>
2020-09-29 07:37:51 +0200

    Remove whole-line-or-region

--- init.el
+++ init.el
@@ -2040,12 +2040,6 @@ I.e., the keyring has a public key for each recipient."
 (use-package nix-mode
   :hook ((proced-mode . nix-prettify-mode)))
 
-(use-package whole-line-or-region
-  :demand t
-  :config
-  (progn
-    (whole-line-or-region-global-mode)))
-
 ;; envrc must come late in the init.el file so add-hook adds it first
 ;; in `find-file-hook'.
 (use-package envrc
