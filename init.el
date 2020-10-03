1ba80fe4678b3408a112f6f26f06eccacc7b3cd8
Damien Cassou <damien@cassou.me>
2020-10-03 12:46:28 +0200

    finsit-prodigy: Fix IP of Gnome Box

--- init.el
+++ init.el
@@ -1665,7 +1665,7 @@ I.e., the keyring has a public key for each recipient."
   :after prodigy
   :config
   (progn
-    (add-to-list 'finsit-prodigy-remotes '("boxes" "http://192.168.122.159:80"))
+    (add-to-list 'finsit-prodigy-remotes '("boxes" "http://192.168.122.114:80"))
     (add-to-list 'finsit-prodigy-remotes '("local" "http://localhost:5000"))
     (finsit-prodigy-setup)))
 
