f76abf1327afae4637f29de32bbd2b1352832ca8
Damien Cassou <damien@cassou.me>
2021-02-24 08:15:45 +0100

    lsp: Reactivate LSP for haskell

--- init.el
+++ init.el
@@ -1826,8 +1826,7 @@ I.e., the keyring has a public key for each recipient."
   :commands (json-navigator-navigate-region json-navigator-navigate-after-point))
 
 (use-package lsp-mode
-  :disabled t
-  :hook (((js2-mode haskell-mode haskell-literate-mode csharp-mode) . lsp)
+  :hook (((haskell-mode haskell-literate-mode csharp-mode) . lsp)
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp
   :load-path "./lib/lsp-mode/clients"
