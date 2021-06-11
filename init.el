9fdeafa6ae57e45b95c315664e9e26eca40fb501
Damien Cassou <damien@cassou.me>
2021-06-11 09:06:43 +0200

    consult: Prevent some commands from automatic candidate preview

--- init.el
+++ init.el
@@ -1644,6 +1644,13 @@ because slides don't change their ID all the time."
          ([remap project-find-regexp] . consult-ripgrep))
   :init
   (progn
+    ;; Configure automatic preview of candidates
+    (consult-customize
+     consult-ripgrep consult-git-grep consult-grep
+     consult-bookmark consult-recent-file consult-xref
+     consult--source-file consult--source-project-file consult--source-bookmark
+     :preview-key (kbd "M-."))
+
     ;; Use Consult to select xref locations with preview
     (setq xref-show-xrefs-function #'consult-xref)
     (setq xref-show-definitions-function #'consult-xref)
