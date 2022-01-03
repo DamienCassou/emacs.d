6698ebfa8abaa70b6d5571d7d8ff8f417a42b952
Damien Cassou <damien@cassou.me>
2022-01-03 14:02:50 +0100

    finsit-magit: Change project

--- init.el
+++ init.el
@@ -1431,7 +1431,7 @@ because slides don't change their ID all the time."
   :after magit
   :config
   (progn
-    (add-to-list 'finsit-magit-projects '("Period report refactoring" . "24152873"))
+    (add-to-list 'finsit-magit-projects '("KPI picker refactoring" . "25577436"))
 
     (finsit-magit-setup)))
 
