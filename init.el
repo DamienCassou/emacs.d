0b1d1497a3dfb7c6b1139738c6bc8162d847b6ec
Damien Cassou <damien@cassou.me>
2022-04-16 16:01:20 +0200

    nano-modeline: Make errors more visible

--- init.el
+++ init.el
@@ -646,7 +646,7 @@ This is recommended by Vertico's README."
 
     (setq nano-modeline-prefix 'icon)
 
-    (set-face-attribute 'nano-modeline-active-primary nil :foreground "#ef4f54")
+    (set-face-attribute 'nano-modeline-active-primary nil :background "#ef4f54")
     (set-face-attribute 'nano-modeline-active-status-** nil :background "#e4c340")
 
     (nano-modeline-mode)))
