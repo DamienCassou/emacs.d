2103b9a5b287b92306882a534f7f5d3fd1ab5699
Damien Cassou <damien@cassou.me>
2022-06-10 20:22:22 +0200

    embark: Use which-key to list embark actions

--- init.el
+++ init.el
@@ -2081,7 +2081,43 @@ the buffer's filename."
          :map embark-file-map
          ("v" . shell-switcher-open-on-directory)
          :map embark-bookmark-map
-         ("v" . shell-switcher-open-on-bookmark)))
+         ("v" . shell-switcher-open-on-bookmark))
+  :init
+  (progn
+    ;; Pressing C-h after a prefix key lists all suffixes with
+    ;; completion:
+    (setq prefix-help-command #'embark-prefix-help-command))
+  :config
+  (progn
+    ;; https://github.com/oantolin/embark/wiki/Additional-Configuration#use-which-key-like-a-key-menu-prompt
+    (defun my/embark-which-key-indicator ()
+      "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+      (lambda (&optional keymap targets prefix)
+        (if (null keymap)
+            (which-key--hide-popup-ignore-command)
+          (which-key--show-keymap
+           (if (eq (plist-get (car targets) :type) 'embark-become)
+               "Become"
+             (format "Act on %s '%s'%s"
+                     (plist-get (car targets) :type)
+                     (embark--truncate-target (plist-get (car targets) :target))
+                     (if (cdr targets) "…" "")))
+           (if prefix
+               (pcase (lookup-key keymap prefix 'accept-default)
+                 ((and (pred keymapp) km) km)
+                 (_ (key-binding prefix 'accept-default)))
+             keymap)
+           nil nil t (lambda (binding)
+                       (not (string-suffix-p "-argument" (cdr binding))))))))
+
+    (setq embark-indicators
+          '(embark--vertico-indicator
+            my/embark-which-key-indicator
+            embark-highlight-indicator
+            embark-isearch-highlight-indicator))))
 
 (use-package embark-consult
   :demand t
