47d1b6b9945f6518b886223c9079375fe5a995a2
Damien Cassou <damien@cassou.me>
2023-09-02 19:09:17 +0200

    eglot: Activate in nix

--- init.el
+++ init.el
@@ -1987,7 +1987,7 @@ If PROJECT is nil, use `project-current'."
 
 (use-package eglot
   :hook ((eglot-managed-mode . my/eglot-setup)
-         ((bash-ts-mode yaml-ts-mode dockerfile-ts-mode graphviz-dot-mode json-js-mode) . eglot-ensure))
+         ((bash-ts-mode yaml-ts-mode dockerfile-ts-mode graphviz-dot-mode json-js-mode nix-ts-mode) . eglot-ensure))
   :config
   (progn
     (defun my/eglot-setup ()
