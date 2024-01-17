b3f0b0128383de1b0195d158d35a82d1f611ae2b
Damien Cassou <damien@cassou.me>
2024-01-17 21:04:36 +0100

    flymake-eslint: Go back to using the globally-installed eslint_d
    eslint_d is faster than eslint and this is important for flymake.

--- init.el
+++ init.el
@@ -1938,24 +1938,14 @@ This should be used as an override of `finsit-js-flycheck-setup'.")
   :init
   (progn
     (setq flymake-eslint-executable-args '("--report-unused-disable-directives"))
+    (setq flymake-eslint-executable-name "eslint_d")
 
     (defun my/flymake-eslint-finsit ()
       "Enable flymake-eslint in finsit's Client/ buffer."
       (when (finsit-core-own-javascript-buffer-p)
         (setq-local flymake-eslint-project-root (finsit-core-monitor-client-location)))
       (when (and (buffer-file-name) (not (derived-mode-p 'json-mode 'json-ts-mode)))
-        (flymake-eslint-enable))))
-  :config
-  (progn
-    (defun my/flymake-eslint--set-executable-name ()
-      "Set `flymake-eslint-executable-name' to the binary in node_modules/."
-      (when-let* (((or (not flymake-eslint-executable-name) (string= flymake-eslint-executable-name "eslint")))
-                  (relative-path "node_modules/.bin/eslint")
-                  (path (locate-dominating-file default-directory relative-path)))
-        (setq flymake-eslint-executable-name (expand-file-name relative-path path))))
-
-    (advice-add #'flymake-eslint--ensure-binary-exists
-                :before #'my/flymake-eslint--set-executable-name)))
+        (flymake-eslint-enable)))))
 
 (use-package eslint-disable-rule
   :demand t
