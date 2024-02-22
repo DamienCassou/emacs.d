512ced06a81a1fbe5b0cb655398531af4ab7fe1b
Damien Cassou <damien@cassou.me>
2024-02-22 20:26:29 +0100

    Assimilate cc-isearch-menu 1.0.3

--- init.el
+++ init.el
@@ -277,92 +277,17 @@ This is recommended by Vertico's README."
       (advice-add #'ffap-menu-ask :around #'my/ffap-menu-ask))))
 
 (use-package isearch
-  :bind (
-         :map isearch-mode-map
-         ("<f2>" . my/isearch-menu))
   :init
   (progn
     (setq isearch-allow-motion t)
-    (setq isearch-lazy-count t))
-  :config
+    (setq isearch-lazy-count t)))
+
+(use-package cc-isearch-menu
+  :demand t
+  :after isearch
+  :init
   (progn
-    (require 'transient)
-    ;; https://github.com/kickingvegas/cclisp/blob/main/cc-isearch-menu.el
-    (transient-define-prefix my/isearch-menu ()
-      "isearch Menu"
-      [["Edit Search String"
-        ("e"
-         "Edit the search string (recursive)"
-         isearch-edit-string
-         :transient nil)
-        ("w"
-         "Pull next word or character word from buffer"
-         isearch-yank-word-or-char
-         :transient nil)
-        ("s"
-         "Pull next symbol or character from buffer"
-         isearch-yank-symbol-or-char
-         :transient nil)
-        ("l"
-         "Pull rest of line from buffer"
-         isearch-yank-line
-         :transient nil)
-        ("y"
-         "Pull string from kill ring"
-         isearch-yank-kill
-         :transient nil)
-        ("t"
-         "Pull thing from buffer"
-         isearch-forward-thing-at-point
-         :transient nil)]
-
-       ["Replace"
-        ("q"
-         "Start ‘query-replace’"
-         isearch-query-replace
-         :if-nil buffer-read-only
-         :transient nil)
-        ("x"
-         "Start ‘query-replace-regexp’"
-         isearch-query-replace-regexp
-         :if-nil buffer-read-only
-         :transient nil)]]
-
-      [["Toggle"
-        ("X"
-         "Toggle regexp searching"
-         isearch-toggle-regexp
-         :transient nil)
-        ("S"
-         "Toggle symbol searching"
-         isearch-toggle-symbol
-         :transient nil)
-        ("W"
-         "Toggle word searching"
-         isearch-toggle-word
-         :transient nil)
-        ("F"
-         "Toggle case fold"
-         isearch-toggle-case-fold
-         :transient nil)
-        ("L"
-         "Toggle lax whitespace"
-         isearch-toggle-lax-whitespace
-         :transient nil)]
-
-       ["Misc"
-        ("o"
-         "occur"
-         isearch-occur
-         :transient nil)
-        ("h"
-         "highlight"
-         isearch-highlight-regexp
-         :transient nil)
-        ("H"
-         "highlight lines"
-         isearch-highlight-lines-matching-regexp
-         :transient nil)]])))
+    (bind-key "<f2>" #'cc-isearch-menu-transient isearch-mode-map)))
 
 (use-package dabbrev
   :bind (("M-/" . nil)
