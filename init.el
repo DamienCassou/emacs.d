399f907b562924c691b7083233dfdbfbf958743d
Damien Cassou <damien@cassou.me>
2024-08-13 14:16:15 +0200

    flymake-eslint: use globally installed eslint_d
    I don't need my fork now that the desired feature has been merged.

--- init.el
+++ init.el
@@ -1908,7 +1908,7 @@ negative, the password is inserted at point."
 (use-package flymake-eslint
   :init
   (progn
-    (setq flymake-eslint-executable-name "/home/cassou/Documents/projects/javascript/eslint_d.js/bin/eslint_d.js")))
+    (setq flymake-eslint-executable-name "eslint_d")))
 
 (use-package reformatter
   :demand t
