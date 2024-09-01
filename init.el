cfebf5c9f6d6706ff70f2826bc463553197e051c
Damien Cassou <damien@cassou.me>
2024-09-01 10:35:59 +0200

    reformatter: specifying the working-directory is important for eslint

--- init.el
+++ init.el
@@ -1922,7 +1922,8 @@ negative, the password is inserted at point."
     (reformatter-define eslintd-fix
       :program "eslint_d"
       :args (list "--fix-to-stdout" "--stdin" "--stdin-filename" (buffer-file-name))
-      :input-file (reformatter-temp-file-in-current-directory "js"))
+      :input-file (reformatter-temp-file-in-current-directory "js")
+      :working-directory (locate-dominating-file default-directory "package.json"))
 
     (reformatter-define prettier
       :program (let* ((root (locate-dominating-file default-directory "node_modules/.bin/prettier"))
