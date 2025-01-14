0fe69fdad191bf767cb6ef34969c1938b03198d4
Damien Cassou <damien@cassou.me>
2025-01-14 12:57:42 +0100

    Try commiting bookmark-default.el to see how that goes

---
+++ var/bookmark-default.el
@@ -0,0 +1,50 @@
+;;;; Emacs Bookmark Format Version 1;;;; -*- coding: no-conversion; mode: lisp-data -*-
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("liste-courses" (filename . "~/personal/listes/course.lst")
+ (front-context-string . "[ courses ]\n\nfar") (rear-context-string)
+ (position . 1) (last-modified 26452 6325 964115 510000))
+("NEWS"
+ (filename
+  . "/nix/store/bxpcj8nbaa274s8h0sxnpjcc4q84hmpm-emacs-unstable-28.1.91/share/emacs/28.1.91/etc/NEWS")
+ (front-context-string . "** Bookmarks\n\n**")
+ (rear-context-string . "\nused instead.\n\n") (position . 61756))
+("org-refile-last-stored"
+ (filename . "~/personal/notes/org/inbox.org")
+ (front-context-string . "** TODO Chercher")
+ (rear-context-string . "n 20:55]\n:END:\n\n") (position . 1765)
+ (last-modified 26501 26705 648787 23000))
+("Avy can do anything | Karthinks"
+ (front-context-string . "What\342\200\231s the Point")
+ (rear-context-string . "n these jumps.\n\n") (position . 32982)
+ (location
+  . "https://karthinks.com/software/avy-can-do-anything/#run-a-skein-through-emacs-help-systems")
+ (handler . eww-bookmark-jump))
+("denote" (filename . "~/configuration/denote/")
+ (front-context-string . "2021-01-17-robor")
+ (rear-context-string . "50 Jun 19 19:07 ") (position . 136))
+("org-capture-last-stored"
+ (filename . "~/personal/notes/org/tickler.org")
+ (front-context-string . "** aller cherche")
+ (rear-context-string . "2025-02-10 Mon>\n") (position . 1327)
+ (last-modified 26501 31661 319553 250000))
+("2021-achat-brest"
+ (filename . "~/famille/logements/2021-achat-brest/")
+ (front-context-string . "2021-11-15-devis")
+ (rear-context-string . "4K Nov 15 19:05 ") (position . 3208))
+("ledger-accounts.gpg"
+ (filename . "~/.password-store/Secure_Notes/ledger-accounts.gpg")
+ (front-context-string . "asset:shares:ban")
+ (rear-context-string . "op\"))\n        (\"") (position . 1020))
+("ftgp-monitor-root"
+ (filename . "~/Documents/projects/ftgp/finsit/monitor/")
+ (front-context-string . "clients\n  drwxr-")
+ (rear-context-string . "20 Sep  8  2016 ") (position . 103)
+ (last-modified 25905 35158 204592 212000))
+("ftgp-monitor-client"
+ (filename . "~/Documents/projects/ftgp/finsit/frontend/apps/client/")
+ (front-context-string . "_\n  -rw-r--r-- 1")
+ (rear-context-string . "36 Sep 16  2021 ") (position . 133)
+ (last-modified 26236 2729 945534 395000))
+)
