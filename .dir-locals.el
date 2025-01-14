d1f1e0c7378d4f286215bab448097253d74f7956
Damien Cassou <damien@cassou.me>
2025-01-14 12:58:33 +0100

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-local-words . "ispell ispell's LocalWords"))))
