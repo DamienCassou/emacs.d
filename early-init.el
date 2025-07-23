1747288bd68c47f1c059672870653558eb8d456d
Damien Cassou <damien@cassou.me>
2025-07-23 20:48:41 +0200

    darwin: Remove unneeded file check

--- early-init.el
+++ early-init.el
@@ -41,5 +41,4 @@
   (shell-command "ls ~/Desktop")
   (shell-command "ls ~/Downloads")
   (shell-command "ls ~/Movies/TV")
-  (shell-command "ls ~/Pictures/'Photo Booth Library'")
-  (shell-command "ls ~/Library/Containers/com.apple.systempreferences.KeyboardSettingsExtension/.com.apple.containermanagerd.metadata.plist"))
+  (shell-command "ls ~/Pictures/'Photo Booth Library'"))
